5bb9cf88ac9abfd8b846c17aa466ad4c812d4c08
khalid <khalid.mohamed@moringaschool.com>
2023-10-01 13:56:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
+
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => response.json())
+    .then((data) => renderBooks(data));
+}
+
+
+function renderBooks(booksData) {
+  const booksList = document.getElementById('books');
+
+  booksData.forEach((book) => {
+    const bookTitle = document.createElement('li');
+    bookTitle.innerText = book.name;
+    booksList.appendChild(bookTitle);
+  });
+}
